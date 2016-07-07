efba78b3b81bab5e1ac288217e7144b55d41dac1
Elliott McNary <El_Is_Based@MacBook-Pro.local>
2016-07-07 09:39:43 -0400

    working well for 4/4 beats and some harder kicks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var audio;
   var biquadFilter;
   var clientParameter = "client_id=033e31e2d036e02f39242e1aa1dd2fa9";
-  var trackPermalinkUrl = "https://soundcloud.com/harshrecordslabel/starx-kyoto-blarax-fvck-it-original-mix";
+  var trackPermalinkUrl = "https://soundcloud.com/apollyonrecords/iamunpossible-x-cupidz-ghastly";
 
   //Get Audio element
   audio = document.getElementById("theSong");
@@ -20,7 +20,7 @@ $(document).ready(function(){
     request.onload = function(){
       var bufferArray = request.response;
       var byteArray = new Uint8Array(bufferArray);
-      var songDurationInSamples = 44100 * audio.duration;;
+      var songDurationInSamples = 44100 * audio.duration;
       //create offline to do the whole song
       var offlineContext = new OfflineAudioContext(1, songDurationInSamples , 44100) || new webkitOfflineAudioContext(1, 2, 44100);
       offlineContext.decodeAudioData(bufferArray, function(buffer){
@@ -48,21 +48,15 @@ $(document).ready(function(){
         offlineContext.oncomplete = function(e) {
           // Filtered buffer!
           var filteredBuffer = e.renderedBuffer;
-          var threshold = 0.90;
+          var threshold = 0.98;
           var pcmArray = filteredBuffer.getChannelData(0);
-          do {
-            var peaks = getPeaksAtThreshold(pcmArray, threshold)
-            threshold -= 0.01;
-          } while(peaks.length < 30 && threshold >= 0.3)
-          console.log(threshold)
           var peaksArr = getPeaksAtThreshold(pcmArray, threshold);
           var timeBetweenPeaks = durationBetweenPeaks(peaksArr, filteredBuffer);
           var timeBetweenPeaksInSeconds = peakDurationInSeconds(timeBetweenPeaks, filteredBuffer);
           var avgTime = getAvgTime(timeBetweenPeaksInSeconds);
+          var bpm = returnBPM(avgTime);
+          console.log(bpm);
         };
-
-        // console.log(buffer.getChannelData(0))
-
       });
     };
     request.send();
@@ -70,18 +64,15 @@ $(document).ready(function(){
 
   function getPeaksAtThreshold(dataArr, threshold){
     var arr = [];
-    console.log(dataArr)
-    var negativeThresh = threshold - 2;
     for(var i = 0; i < dataArr.length;){
-      if(dataArr[i] > threshold || dataArr[i] < negativeThresh){
+      if(dataArr[i] > threshold){
         arr.push(i);
         i += 10000;
       }
       i++;
     }
-    console.log(arr);
     return arr;
-  };
+  }
 
   function durationBetweenPeaks(peaksArr, buffer){
     var arr = [];
@@ -92,7 +83,7 @@ $(document).ready(function(){
       }
     }
     return arr;
-  };
+  }
 
   function peakDurationInSeconds(durationArr, buffer){
     var lengthOfBufferIndex = buffer.duration / buffer.length;
@@ -102,27 +93,34 @@ $(document).ready(function(){
       arr.push(timeInSeconds);
     });
     return arr;
-  };
+  }
 
   function getAvgTime(arr){
     var intervals = [];
-    var threshold = 0.2;
+    //closeness of neighbors
+    var threshold = 0.02;
     arr.forEach(function(peak, index){
       var counter = 0;
       for(var i=0; i < 10; i++){
+        //if neighbors are close we have a "true peak"
         if(arr[index + i] - arr[index] < threshold){
           counter ++;
         }
       }
-      if(counter > 8){
+      //9 is the threshold for true peaks
+      if(counter > 9){
+        //rounded for convenience and matching sake
         var rounded = peak.toFixed(3);
         var foundInterval = false;
+        //look to see if that interval already exists
         intervals.forEach(function(obj){
-          if(obj.interval == rounded){
+          if(obj.interval === rounded){
             foundInterval = true;
+            //increase object count if we already have that interval in the array
             return obj.count++;
           }
         });
+        //no interval, create ne interval obj.
         if(!foundInterval && rounded < 1){
           intervals.push({
             interval: rounded,
@@ -131,10 +129,30 @@ $(document).ready(function(){
         }
       }
     });
+    //sort so the highest counts are at the end
     intervals.sort(function(a, b){
       return a.count - b.count;
     });
-    console.log(intervals);
     return intervals;
+  }
+
+  function returnBPM(arr){
+    var bpmArr = [];
+    //min number of count in arr obj
+    var threshold = 6;
+    //only go through last three objects (greatest counts)
+    for (var i = arr.length - 1; i > arr.length - 4; i--) {
+      if(arr[i].count > threshold){
+        //bpm is 60 seconds / the highest interval thresholds.
+        var bpm = 60 / arr[i].interval;
+        if(bpm < 80){
+          bpm = bpm * 2;
+        }
+        bpm = Math.round(bpm);
+        bpmArr.push(bpm);
+      }
+    }
+    //bpm will have three indexes with the three most likely bpms
+    return bpmArr[0];
   }
 });
